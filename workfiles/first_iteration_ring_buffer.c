37740879b08b4904dafb3e5a18747baeba234d03
tkoetting03 <141885187+tkoetting03@users.noreply.github.com>
2025-10-24 14:46:41 -0500

    Implement basic ring buffer functionality
    Initialize a ring buffer and demonstrate accessing an element.

--- workfiles/first_iteration_ring_buffer.c
+++ workfiles/first_iteration_ring_buffer.c
@@ -1,1 +1,19 @@
 
+#include <stdio.h>
+
+
+int main() {
+
+
+    #define BUFFER_SIZE 16
+    int buffer[BUFFER_SIZE] = {0};
+    int *p = &buffer[0];
+    buffer[1] = 25;
+    int goalPosition = 17;
+    int bufferMask = BUFFER_SIZE - 1;
+    p = &buffer[goalPosition & bufferMask];
+
+    printf("%d\n", *p);
+
+return 0;
+}
