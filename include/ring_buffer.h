1268ab2b42625f4414a566539c69f4665a1da798
tkoetting03 <141885187+tkoetting03@users.noreply.github.com>
2025-11-01 18:34:32 +0000

    Header and source file

---
+++ include/ring_buffer.h
@@ -0,0 +1,21 @@
+#ifndef RING_BUFFER_H
+#define RING_BUFFER_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct ringBuffer ringBuffer;
+
+typedef enum { noError = 0, argError = -1, capError = -2, allocError = -3, fullError = -4, emptyError = -5 } ringError;
+
+int ringSize(ringBuffer *pointerStruct);
+int ringCap(ringBuffer *pointerStruct);
+bool ringFull(ringBuffer *pointerStruct);
+bool ringEmpty(ringBuffer *pointerStruct);
+void ringBuffer_init(ringBuffer *pointerStruct, int capacity);
+void push(ringBuffer *pointerStruct, int pushValue);
+void pop(ringBuffer *pointerStruct, int *outputLocation);
+
+#endif
--- src/ring_buffer.c
+++ src/ring_buffer.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "ring_buffer.h"
+
 
 typedef struct {
     int *buffer;
@@ -80,42 +82,3 @@ void pop(ringBuffer *pointerStruct, int *outputLocation) {
     pointerStruct->stored--;
 }
 
-int ringSize(ringBuffer *pointerStruct) {
-    return pointerStruct->stored;
-}
-
-int ringCap(ringBuffer *pointerStruct) {
-    return pointerStruct->capacity;
-}
-
-bool ringFull(ringBuffer *pointerStruct) {
-    return pointerStruct->capacity == pointerStruct->stored;
-}
-
-bool ringEmpty(ringBuffer *pointerStruct) {
-    return pointerStruct->stored == 0;
-}
-
-int main(void) {
-    const int capacity = 16;  
-
-    ringBuffer rb;
-
-    ringBuffer_init(&rb, capacity);
-
-    for (int i = 0; i < capacity; ++i) {
-        int val = i * 10;
-        push(&rb, val);
-        printf("push: %3d  [head=%2d, tail=%2d, stored=%2d]\n",
-               val, rb.head, rb.tail, rb.stored);
-    }
-    while (rb.stored > 0) {
-        int out = 0;
-        pop(&rb, &out);
-        printf("pop : %3d  [head=%2d, tail=%2d, stored=%2d]\n",
-               out, rb.head, rb.tail, rb.stored);
-    }
-
-    free(rb.buffer);
-    return 0;
-}
